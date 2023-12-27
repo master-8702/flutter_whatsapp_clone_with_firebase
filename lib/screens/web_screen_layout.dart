259a59ce7665ce9e12bab0178059f27aeac3c68f
Ibrahim Selman <ibroselman@gmail.com>
2023-12-27 20:59:27 +0300

    Add web_screen_layout

---
+++ lib/screens/web_screen_layout.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp_clone_with_firebase/common/contacts_list.dart';
+import 'package:flutter_whatsapp_clone_with_firebase/widgets/web_profile_bar.dart';
+import 'package:flutter_whatsapp_clone_with_firebase/widgets/web_search_bar.dart';
+
+class WebScreenLayout extends StatelessWidget {
+  const WebScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Row(
+      children: [
+        const Expanded(
+          child: Column(
+            children: [
+              // web profile bar
+              WebProfileBar(),
+
+              // web search bar
+              WebSearchBar(),
+
+              // contacts list
+              Expanded(child: ContactsList())
+            ],
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image:
+                      AssetImage('assets/images/whatsapp_background_image.png'),
+                  fit: BoxFit.cover)),
+        )
+      ],
+    ));
+  }
+}
