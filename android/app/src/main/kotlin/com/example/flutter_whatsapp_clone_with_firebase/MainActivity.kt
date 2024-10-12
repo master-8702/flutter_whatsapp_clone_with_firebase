1b8829879284f05987bdc6af007b816f87776fc7
master-8702 <ibroselman@gmail.com>
2024-10-12 13:33:20 +0300

    Update gradle version

--- android/app/src/main/kotlin/com/example/flutter_whatsapp_clone_with_firebase/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_whatsapp_clone_with_firebase/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.flutter_whatsapp_clone_with_firebase
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
