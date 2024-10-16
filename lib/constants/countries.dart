deef4641762729f0c3f3be9921bef7b41c1d85b1
master-8702 <ibroselman@gmail.com>
2024-10-16 23:23:33 +0300

    add some sample country list

---
+++ lib/constants/countries.dart
@@ -0,0 +1,93 @@
+const List<Map<String, dynamic>> countryList = [
+  {
+    'flag': 'assets/images/flags/cn.png', 
+    'name': 'China',
+    'localName': '中国',
+    'countryCode': '7'
+  },
+  {
+    'flag': 'assets/images/flags/et.png',
+    'name': 'Ethiopia',
+    'localName': 'ኢትዮጵያ',
+    'countryCode': '251'
+  },
+  {
+    'flag': 'assets/images/flags/in.png',
+    'name': 'India',
+    'localName': 'भारत',
+    'countryCode': '91'
+  },
+  {
+    'flag': 'assets/images/flags/pk.png',
+    'name': 'Pakistan',
+    'localName': 'پاکستان',
+    'countryCode': '92'
+  },
+  {
+    'flag': 'assets/images/flags/ru.png',
+    'name': 'Russia',
+    'localName': 'Россия',
+    'countryCode': '86'
+  },
+  {
+    'flag': 'assets/images/flags/cn.png',
+    'name': 'China2',
+    'localName': '中国',
+    'countryCode': '7'
+  },
+  {
+    'flag': 'assets/images/flags/et.png',
+    'name': 'Ethiopia2',
+    'localName': 'ኢትዮጵያ',
+    'countryCode': '251'
+  },
+  {
+    'flag': 'assets/images/flags/in.png',
+    'name': 'India2',
+    'localName': 'भारत',
+    'countryCode': '91'
+  },
+  {
+    'flag': 'assets/images/flags/pk.png',
+    'name': 'Pakistan2',
+    'localName': 'پاکستان',
+    'countryCode': '92'
+  },
+  {
+    'flag': 'assets/images/flags/ru.png',
+    'name': 'Russia2',
+    'localName': 'Россия',
+    'countryCode': '86'
+  },
+  {
+    'flag': 'assets/images/flags/cn.png',
+    'name': 'China3',
+    'localName': '中国',
+    'countryCode': '7'
+  },
+  {
+    'flag': 'assets/images/flags/et.png',
+    'name': 'Ethiopia3',
+    'localName': 'ኢትዮጵያ',
+    'countryCode': '251'
+  },
+  {
+    'flag': 'assets/images/flags/in.png',
+    'name': 'India3',
+    'localName': 'भारत',
+    'countryCode': '91'
+  },
+  {
+    'flag': 'assets/images/flags/pk.png',
+    'name': 'Pakistan3',
+    'localName': 'پاکستان',
+    'countryCode': '92'
+  },
+  {
+    'flag': 'assets/images/flags/ru.png',
+    'name': 'Russia3',
+    'localName': 'Россия',
+    'countryCode': '86'
+  },
+];
+
